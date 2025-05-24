90ca680901aa9a0284c4694ce6731a32604c801d
Yu Zen <cheahyuzen@gmail.com>
2025-05-24 22:58:36 +0800

    changes made include 1) adding prices for the food items being sold in main.py in the form of a dictionary, 2) total price of items is calculated in main.py n sent to checkout.html for user to review, 3) added checkboxes for user to choose which good they want to order in customer_home.html, 4) added <p> tags to display the items ordered in checkout.html

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+stall_phone_number = None
+
+item_prices = {
+    'Steamed Chicken Rice': 5.00,
+    'Whole Steamed Chicken': 20.00,
+    'Half Steamed Chicken': 15.00
+}
+
 @app.route('/')
 def login():
     return render_template('login.html')
@@ -13,15 +21,35 @@ def login():
 def home():
     return render_template('customer_home.html')
 
+
 # here is where the ngas can see what they ordered
-@app.route('/checkout', methods=['POST'])
+@app.route('/checkout', methods=['GET', 'POST']) # Maybe add a msg as well to say 'u hvnt ordered shi', but not yet focus on basics first
 def checkout():
-    return render_template('checkout.html')
+    if request.method == 'POST':
+        items_ordered = request.form.getlist('items')
+        print(items_ordered)
+        price_list = []
+        total_cost = 0
+        for item in items_ordered:
+            item_price = item_prices[item]
+            price_list.append(item_price)
+            total_cost += item_price
+        return render_template('checkout.html', items_ordered = items_ordered, price_list = price_list, total_cost = total_cost)
+    else:
+        return render_template('customer_home.html')
+
 
 # here is where they scan the qr code and submit ss of paynow
 @app.route('/payment', methods=['POST'])
 def payment():
-    return render_template('payment.html')
+    payment_amount = request.form.getlist('items')
+    return render_template('payment.html', payment_amount = payment_amount)
+    #STOPPPED HEREEE
+
+# this is where the YOLO will take out all the impt info and verify, as well as upload the data to a database
+@app.route('/confirmation', methods=['POST'])
+def confirmation():
+    return render_template('confirmation.html')
 
 # do stall owner stuff only after u finish all the customer stuff
 @app.route('/admin_home', methods=['POST'])
