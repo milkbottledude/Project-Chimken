cf7efb1180e494a15663acda01ebcfc97d83b02f
Yu Zen <cheahyuzen@gmail.com>
2025-05-24 16:52:04 +0000

    first ever commit from gcloud shell editor. Changed from checkboxes, which cannot edit quantity of food item, to input that accepts numbers in customer_home.html. changed main.py to accomodate the food item amounts, and changed checkout.py to display the amount of each food item ordered, as well asthe total cost of each food item type and the final payment amount in payment.html

---
+++ README.md
@@ -0,0 +1,9 @@
+# Project-Chimken
+
+![banner here](progress_pics/Sing_Soon_Lee_Chicken_Rice_Cropped_Banner.png)
+
+- 🗪 Feel free to telegram me [@milkbottledude](https://t.me/milkbottledude) if you have any questions, or just want to chat :)
+
+> TLDR: A poor man's online ordering system webapp :)
+
+It was a normal Saturday as I took my place at the back of the obscenely long queue, which led to my favourite chicken rice stall, Sing Soon Lee Hainanese Chicken Rice. Located in the heart of AMK 628 Market and Food Centre, its fragrant rice and succulent steamed chicken never fail to bring in a crowd of customers. However, while 
---
+++ main.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import numpy as np
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+stall_phone_number = None
+
+item_prices = {
+    'Steamed Chicken Rice': 5.00,
+    'Whole Steamed Chicken': 20.00,
+    'Half Steamed Chicken': 15.00
+}
+
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+# add python code for home below
+@app.route('/home')
+def home():
+    return render_template('customer_home.html')
+
+
+# here is where the ngas can see what they ordered
+@app.route('/checkout', methods=['GET', 'POST']) # Maybe add a msg as well to say 'u hvnt ordered shi', but not yet focus on basics first
+def checkout():
+    if request.method == 'POST':
+        steamed_chicken_rice_ordered = int(request.form.get('steamed_chicken_rice_amt'))
+        whole_steamed_chicken_ordered = int(request.form.get('whole_steamed_chicken_amt'))
+        half_steamed_chicken_ordered = int(request.form.get('half_steamed_chicken_amt'))
+        item_amts_ordered = [steamed_chicken_rice_ordered, whole_steamed_chicken_ordered, half_steamed_chicken_ordered]
+        items = ['Steamed Chicken Rice', 'Whole Steamed Chicken', 'Half Steamed Chicken']
+        items_ordered = []
+        print(item_amts_ordered)
+        for x in range(len(item_amts_ordered)):
+            if item_amts_ordered[x] > 0:
+                items_ordered.append(items[x])
+        price_list = []
+        print(items_ordered)
+        for item in items_ordered:
+            item_base_price = item_prices[item]
+            index = items.index(item)
+            item_price = item_base_price * item_amts_ordered[index]
+            price_list.append(item_price)
+        total_cost = sum(price_list)
+        item_amts_ordered_filtered = [x for x in item_amts_ordered if x > 0]
+        return render_template('checkout.html', items_ordered = items_ordered, price_list = price_list, total_cost = total_cost, item_amts_ordered_filtered = item_amts_ordered_filtered)
+    else:
+        return render_template('customer_home.html')
+
+
+# here is where they scan the qr code and submit ss of paynow
+@app.route('/payment', methods=['POST'])
+def payment():
+    payment_amount = request.form.get('total_cost')
+    print(payment_amount)
+    return render_template('payment.html', payment_amount = payment_amount)
+
+# this is where the YOLO will take out all the impt info and verify, as well as upload the data to a database
+@app.route('/confirmation', methods=['POST'])
+def confirmation():
+    return render_template('confirmation.html')
+
+# do stall owner stuff only after u finish all the customer stuff
+@app.route('/admin_home', methods=['POST'])
+def admin_home():
+    return render_template('admin_home.html')
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
